7aab03ea9dcc0cd9a1b2c05c9ae6022a86f02468
GabboId <75744793+GabboId@users.noreply.github.com>
2024-03-26 18:15:44 +0100

    Update ssi.c
    DoIO (con errori perchè altrimenti non mi piace)

--- phase2/ssi.c
+++ phase2/ssi.c
@@ -29,9 +29,46 @@ void terminateProcess(ssi_payload_PTR p){//elimina o il processo che fa la send 
 
 }
 
-void DoIO(){//TODO
-
+typedef struct WaitingDoIO{
+    pct_PTR pcb;
+    unsigned int device;
 }
+
+//Inizializzare una nuova lista dei processi bloccati per il DoIO. Non la metto in quanto non so se ne è già una presente
+
+void DoIO(){
+
+    insertProcQ(blocked_process, current_process);   
+    insertProcQ(blocked_process_DoIO, (current_process, //Indirizzo device IO)) //So anche che non si può inserire perchè serve un pcb ma non penso sia efficace inserire un nuovo campo su PCB. Dobbiamo per forza creare una nuova lista?
+    current_process = NULL;
+    WAIT();
+
+    unsigned int device_address = //Indirizzo device IO;
+    unsigned int ack_command = ACK;
+    SYSCALL(SENDMESSAGE, device_address, (unsigned int)(&ack_command), 0);
+
+
+    ssi_do_io_t do_io = {
+        .commandAddr = command,
+        .commandValue = value,
+    };
+    ssi_payload_t payload = {
+        .service_code = DOIO,
+        .arg = &do_io,
+    };
+
+    SYSCALL(SENDMESSAGE, (unsigned int)ssi_pcb, (unsigned int)(&payload), 0);
+    SYSCALL(RECEIVEMESSAGE, (unsigned int) ssi_pcb, (unsigned int)(&response), 0);
+
+    WaitingDoIO current_process = headProcQ(blocked_process_DoIO); //Stessa cosa ultimo commento
+    
+    while(WaitingDoIO != NULL && WaitingDoIO->device != device_address){
+        WaitingDoIO = WaitingDoIO->next;
+    }
+    insertProcQ(ready_queue, WaitingDoIO->pcb);
+    removeProcQ(blocked_process_DoIO, WaitingDoIO);
+    //Devo modificare lo stato del pcb? In realtà lo stato non è mai stato in pausa quindi dovrebbe essere ancora in run no?
+ }
 
 signed int GetCPUTime(){//ciclo che prende i p_time da tutti i processi nella ready_queue e restituisce la somma
     pcb_PTR p;
